c50a20e6104bef909dc7bf044053962f5075893a
Andrew Jones <andyjones.ed@gmail.com>
2020-07-29 15:23:24 +0100

    Adds a repo of symbols, fixes distribution issues

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instructions
+1. Install the extension
+2. Type '\alpha'
+3. Hit tab.
+
+## Credit
+[Viktor Qvarfordt](https://github.com/ViktorQvarfordt/unicode-latex)'s repo is the source of the latex -> unicode mappings, and saved me some excruciating work.
--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,12 @@
 import * as vscode from 'vscode';
-
-var SYMBOLS: { [id: string]: string; } = {
-	'alpha': 'α',
-	'beta': 'β'
-};
+import * as SYMBOLS from './latex-unicode.json';
 
 class Provider {
 
 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CompletionItem[]> {
 		var completions: vscode.CompletionItem[] = [];
 		Object.entries(SYMBOLS).forEach((entry) => {
-			let item = new vscode.CompletionItem('\\' + entry[0], vscode.CompletionItemKind.Text);
+			let item = new vscode.CompletionItem(entry[0], vscode.CompletionItemKind.Text);
 			item.insertText = entry[1];
 			// I have no idea what a good regex for LaTeX symbol codes looks like.
 			item.range = document.getWordRangeAtPosition(position, /[\w\\]+/);
